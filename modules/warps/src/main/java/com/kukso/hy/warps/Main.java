5a0f960192529564e78baa085f916357b33bd395
DevBD1 <burakdorman@gmail.com>
2026-01-19 00:35:26 +0300

    refactor: move commands

--- modules/warps/src/main/java/com/kukso/hy/warps/Main.java
+++ modules/warps/src/main/java/com/kukso/hy/warps/Main.java
@@ -1,12 +1,15 @@
 package com.kukso.hy.warps;
 
+import com.kukso.hy.lib.locale.LocaleMan;
+import com.kukso.hy.warps.commands.CmdWrapper;
+import com.kukso.hy.warps.commands.main.DelWarpCommand;
+import com.kukso.hy.warps.commands.main.ListWarpsCommand;
+import com.kukso.hy.warps.commands.main.SetWarpCommand;
+import com.kukso.hy.warps.commands.main.WarpCommand;
+
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.util.Config;
-import com.kukso.hy.warps.commands.DelWarpCommand;
-import com.kukso.hy.warps.commands.ListWarpsCommand;
-import com.kukso.hy.warps.commands.SetWarpCommand;
-import com.kukso.hy.warps.commands.WarpCommand;
 
 import javax.annotation.Nonnull;
 
@@ -22,11 +25,14 @@ public class Main extends JavaPlugin {
     @Override
     protected void setup() {
         super.setup();
-        
+
+        LocaleMan.init(this);
+
         WarpManager warpManager = new WarpManager(this.config);
-        
+
         this.getCommandRegistry().registerCommand(new WarpCommand(warpManager));
         this.getCommandRegistry().registerCommand(new SetWarpCommand(warpManager));
+//        this.getCommandRegistry().registerCommand(new CmdWrapper(new SetWarpCommand(warpManager)));
         this.getCommandRegistry().registerCommand(new DelWarpCommand(warpManager));
         this.getCommandRegistry().registerCommand(new ListWarpsCommand(warpManager));
     }
